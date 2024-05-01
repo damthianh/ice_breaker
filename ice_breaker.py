aa8018a753e50908a1465b652d73ece8e1c3a39e
THIADAM <damanh1005@gmail.com>
2024-05-01 21:35:44 +0200

    create the first app

--- ice_breaker.py
+++ ice_breaker.py
@@ -1,17 +1,17 @@
 from dotenv import load_dotenv
+from typing import Tuple
 import os
 from langchain.prompts.prompt import PromptTemplate
 from langchain_openai import ChatOpenAI
-from langchain.chains import LLMChain
 
-from output_parsers import summary_parser
+from output_parsers import summary_parser, Summary
 from third_parties.linkedin import scrape_linkedin_profile
 from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
 
 
-def ice_breaker_with(name: str) -> str:
+def ice_breaker_with(name: str) -> Tuple[Summary, str]:
     linkedin_username = linkedin_lookup_agent(name=name)
-    linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_username)
+    linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_username, mock=True)
     summary_template = """
     given the Linkedin information {information} about a person I want you to create:
     1. A short summary
@@ -24,10 +24,9 @@ def ice_breaker_with(name: str) -> str:
                                                  "format_instructions": summary_parser.get_format_instructions()
                                              })
     llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
-    #chain = LLMChain(llm=llm, prompt=summary_prompt_template)
     chain = summary_prompt_template | llm | summary_parser
-    res = chain.invoke(input={"information": linkedin_data})
-    print(res)
+    res: Summary = chain.invoke(input={"information": linkedin_data})
+    return res, linkedin_data.get("profile_pic_url")
 
 
 if __name__ == "__main__":
